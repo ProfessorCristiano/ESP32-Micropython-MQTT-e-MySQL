0485ef4d293ed1e349e02c3f32f7afa1c928410b
Cristiano Teixeira <prof.cristiano.info@gmail.com>
2025-10-08 21:21:24 -0300

    Update cliente-mqtt.py

--- cliente-mqtt.py
+++ cliente-mqtt.py
@@ -1,19 +1,33 @@
-import asyncio
-from asyncio_mqtt import Client
+# pip install typing_extensions
+# pip install aiomqtt[all]
 
+import asyncio
+import sys
+import os
+from aiomqtt import Client
+
+# Corrigir o loop no Windows
+if sys.platform.lower() == "win32" or os.name.lower() == "nt":
+    from asyncio import set_event_loop_policy, WindowsSelectorEventLoopPolicy
+    set_event_loop_policy(WindowsSelectorEventLoopPolicy())
+
+# Configurações do MQTT
 TOPIC = 'Estufa'
 BROKER = 'broker.hivemq.com'
 PORT = 1883
 
 async def main():
-    async with Client(BROKER, PORT) as client:
-        await client.subscribe(TOPIC)
-        print(f'Subscrito ao tópico {TOPIC}')
+    try:
+        async with Client(BROKER, PORT) as client:
+            await client.subscribe(TOPIC)
+            print(f'Subscrito ao tópico {TOPIC}')
 
-        async with client.messages() as messages:
-            async for message in messages:
+            async for message in client.messages:
                 print(f'Mensagem recebida no tópico {message.topic}: {message.payload.decode()}')
-                # acrescentar depois o código para  extrair as informações do message.payload.decode() e enviar para o mysql.
+                # Aqui você pode adicionar o código para extrair e enviar os dados para o MySQL
+    except Exception as e:
+        print(f'Erro inesperado: {e}')
 
 if __name__ == '__main__':
     asyncio.run(main())
+
